361e47c864b10eaabee997517a6dcdd915c9d760
Dawid Urbański <hi@dawidurbanski.com>
2024-04-20 21:16:43 +0200

    Basic plugin admin notice (#3)

--- plugins/basic-plugin/basic-plugin.php
+++ plugins/basic-plugin/basic-plugin.php
@@ -2,3 +2,7 @@
 /**
  * Plugin Name: Basic Plugin from Monorepo
  */
+
+add_action('admin_notices', function() {
+    echo '<div class="notice notice-success is-dismissible"><p>Basic Plugin from Monorepo is active</p></div>';
+});
